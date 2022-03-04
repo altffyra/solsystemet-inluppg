7cfea9641929ee2df3bac2a06aca9990d40ecc8f
Christian Johnsson <chjohnsson@hotmail.com>
2022-03-04 10:57:37 +0100

    smink

--- modules/getbodies.js
+++ modules/getbodies.js
@@ -4,9 +4,7 @@ import {divideData} from "../index.js"
 
 async function bodiesData(APIKEY){
 let response = await fetch('https://fathomless-shelf-54969.herokuapp.com/bodies', 
-    {method: 'GET',
-    headers: {'x-zocom': APIKEY }
-})
+    {method: 'GET', headers: {'x-zocom': APIKEY }})
 let data = await response.json();
 let allPlanets = data.bodies;
 divideData(allPlanets);
